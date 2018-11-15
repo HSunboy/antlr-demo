6d7f726b684d65478bfb0bdd82ec0716a5765148
HSunboy <nolanctt@hotmail.com>
2018-11-15 13:40:31 +0800

    add spark demo

---
+++ index.js
@@ -0,0 +1,28 @@
+const antlr4 = require("antlr4");
+const sparkLexer = require("./parser/spark/grammar/sparkLexer").sparkLexer;
+const sparkParser = require("./parser/spark/grammar/sparkParser").sparkParser;
+const sparkListener = require("./parser/spark/grammar/sparkListener").sparkListener;
+
+const input = "SELECT * FROM TBAL"
+const chars = new antlr4.InputStream(input);
+const lexer = new sparkLexer(chars);
+const tokens = new antlr4.CommonTokenStream(lexer);
+const parser = new sparkParser(tokens);
+parser.buildParseTrees = true;
+const tree = parser.singleStatement();
+const KeyPrinter = function () {
+    sparkListener.call(this); // inherit default listener
+    return this;
+};
+
+// continue inheriting default listener
+KeyPrinter.prototype = Object.create(sparkListener.prototype);
+KeyPrinter.prototype.constructor = KeyPrinter;
+
+// override default listener behavior
+KeyPrinter.prototype.enterSingleStatement=function(ctx){
+    console.log(ctx)
+}
+const printer = new KeyPrinter();
+antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer, tree);
+// console.log(tree);
